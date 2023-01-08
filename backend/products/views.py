433b53497a283f501df0d9cb328c2a93a36e0f64
Akolade <emmakolade@gmail.com>
2023-01-08 14:09:48 +0100

    list and detail view

--- backend/products/views.py
+++ backend/products/views.py
@@ -23,7 +23,28 @@ class ProductListCreateAPIView(ListCreateAPIView):
 class ProductDetailAPIView(RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    
-    
 
-def product_alt_view
+
+@api_view
+def product_alt_view(request, pk=None, * args, **kwargs):
+    if request.method == 'GET':
+        if pk is not None:
+            obj = get_object_or_404(Product, pk=pk)
+            data = ProductSerializer(obj, many=False).data
+            return Response(data)
+
+        qs = Product.objects.all()
+        data = ProductSerializer(qs, many=True).data
+        return Response(data)
+
+    if request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+    if serializer.is_valid(raise_exception=True):
+
+        title = serializer.validated_data.get('title')
+        content = serializer.validated_data.get('content') or None
+        if content is None:
+            content = title
+        serializer.save(content=content)
+        return Response(serializer.data)
+    return Response({"invalid": "not good data"}, status=400)
